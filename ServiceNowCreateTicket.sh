695ac8212d10d60082c2df3628a09f3442a9b567
KenEversource <44266720+KenEversource@users.noreply.github.com>
2018-10-19 15:49:29 -0400

    arg

--- ServiceNowCreateTicket.sh
+++ ServiceNowCreateTicket.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 echo "Create a Ticket in Service Now"
-
+echo "First arg: $0"
+echo "Second arg: $1"
 curl  -i -H "Content-type: application/json" -H "Accept: application/json" https://eversourcetest.service-now.com/api/now/v1/table/incident/16d04292db856f006f9b6f13ca961982
