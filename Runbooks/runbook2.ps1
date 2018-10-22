28d479ded24f4f9babb0a7a6f638931a21614e11
KenEversource <44266720+KenEversource@users.noreply.github.com>
2018-10-22 13:47:09 -0400

    a new commit to runbook

--- Runbooks/runbook2.ps1
+++ Runbooks/runbook2.ps1
@@ -5,7 +5,7 @@ workflow runbook2 {
         # If you do not enter anything, the default value of Name 
         # will be World
         [parameter(Mandatory=$false)]
-        [String]$Name = "World2 test"
+        [String]$Name = "World2 test 2"
     )
 
         Write-Output "Hello $Name"
