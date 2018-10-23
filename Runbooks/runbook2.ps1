bce4c0cc3c120f233ad58b792eea2bed7b2c6102
KenEversource <44266720+KenEversource@users.noreply.github.com>
2018-10-23 08:30:37 -0400

    webhook runbook

--- Runbooks/runbook2.ps1
+++ Runbooks/runbook2.ps1
@@ -1,13 +1,9 @@
 workflow runbook2 {
     param (
-        
-        # Optional parameter of type string. 
-        # If you do not enter anything, the default value of Name 
-        # will be World
         [parameter(Mandatory=$false)]
-        [String]$Name = "World2 test webhook"
+        [object]$WebHookData
     )
 
-        Write-Output "Hello $Name"
+    Write-Output "Hello $WebHookData"
 
 }
