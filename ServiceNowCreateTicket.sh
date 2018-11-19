2d535fd12d8cd1b74ef854d6a50c7f27671aeb85
KenEversource <44266720+KenEversource@users.noreply.github.com>
2018-11-19 15:17:21 -0500

    fix args

--- ServiceNowCreateTicket.sh
+++ ServiceNowCreateTicket.sh
@@ -2,4 +2,4 @@
 echo "Create a Ticket in Service Now"
 echo "First arg: $0"
 echo "Second arg: $1"
-curl -d '{"key1":"value1", "key2":"value3"}' -H "Content-Type: application/json" -X POST azurewebhook
+curl -d '{"key1":"value1", "key2":"value3"}' -H "Content-Type: application/json" -X POST $1
